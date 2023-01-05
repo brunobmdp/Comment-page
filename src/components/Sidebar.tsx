78941e34007cd38bd20d9cf6fe6103db23297f56
brunobmdp <brunobmdp@gmail.com>
2023-01-05 10:20:53 -0300

    Post page using ReactJS

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,26 @@
+import { PencilLine } from 'phosphor-react'
+import { Avatar } from './Avatar';
+import styles from './Sidebar.module.css';
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1550745165-9bc0b252726f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"
+      />
+
+      <div className={styles.profile}>
+        <Avatar src="https://github.com/brunobmdp.png" />
+        <strong>Bruno Marques</strong>
+        <span>web developer</span>
+      </div>
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
+}
